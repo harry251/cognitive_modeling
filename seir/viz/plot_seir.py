69d5c331e3b2eba0cc1e1ead39ecf1508cebd5d6
Byungyeon Kim <harrykim030827@gmail.com>
2025-01-23 17:25:41 -0500

    [seir] Remove hard-coded values within plot_seir.py

--- seir/viz/plot_seir.py
+++ seir/viz/plot_seir.py
@@ -1,13 +1,19 @@
 import matplotlib.pyplot as plt
 
-
-def plot_results(infected):
+def plot_results(infected, 
+    figsize=(10, 6),
+    color='#AA0000',
+    linestyle='dashed',
+    marker='o', 
+    labelfontsize=16,
+    titlefontsize=20,
+    title='Simulated Oubreak'):
     """Plots the time series of infected cases."""
     
-    fig, ax = plt.subplots(1, 1, figsize=(10, 6))
-    ax.plot(infected, color='#AA0000', linestyle='dashed', marker='o')
-    ax.set_xlabel('Day', fontsize=16)
-    ax.set_ylabel('Number of Infected Cases', fontsize=16)
-    ax.set_title('Simulated Oubreak', fontsize=20)
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+    ax.plot(infected, color=color, linestyle=linestyle, marker=marker)
+    ax.set_xlabel('Day', fontsize=labelfontsize)
+    ax.set_ylabel('Number of Infected Cases', fontsize=labelfontsize)
+    ax.set_title(title, fontsize=titlefontsize)
     ax.grid(alpha=0.2)
     return fig
