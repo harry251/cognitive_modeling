7b0a685a87b4a234fe788b389cd5185d0c7350fd
Byungyeon Kim <harrykim030827@gmail.com>
2025-01-24 23:20:34 -0500

    [seir] Add proper type hints in plot_seir.py

--- seir/viz/plot_seir.py
+++ seir/viz/plot_seir.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
+from typing import Sequence, Tuple
 
-def plot_results(infected, 
-    figsize=(10, 6),
-    color='#AA0000',
-    linestyle='dashed',
-    marker='o', 
-    labelfontsize=16,
-    titlefontsize=20,
-    title='Simulated Oubreak'):
+def plot_results(
+    infected: Sequence[float], 
+    figsize: Tuple[int, int] = (10, 6),
+    color: str = '#AA0000',
+    linestyle: str = 'dashed',
+    marker: str = 'o', 
+    labelfontsize: int = 16,
+    titlefontsize: int = 20,
+    title: str = 'Simulated Oubreak'):
     """Plots the time series of infected cases."""
     
     fig, ax = plt.subplots(1, 1, figsize=figsize)
